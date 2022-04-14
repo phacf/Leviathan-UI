81a0c88aff59e377543526f47444fa26bda6ea94
paulo.fernandes <paulo.fernandes@55pbx.com.br>
2022-04-14 14:35:53 -0300

    add types

--- src/components/types.ts
+++ src/components/types.ts
@@ -2,6 +2,8 @@ export interface StyleTypes {
     width?: string;
     height?: string;
     padding?: string;
+	maxHeight?: string;
+	maxWidth?: string;
     
     backgroudColor?: string;
 
@@ -11,7 +13,30 @@ export interface StyleTypes {
     fontSize?: string;
     fontColor?: string;
 
+    
+    position?: string;
+	top?: string;
+	bottom?: string;
+	left?: string;
+	right?: string;
+    
+	fontWeight?: string;
+    
+	cursor?: string;
+    
+	float?: string;
+
     display?:string;
+	alignSelf?: string;
+	justifyContent?: string;
+	justifySelf?: string;
+	alignItems?: string;
+	flexDirection?: string;
+
+	margin?: string;
+	id?: string;
+	src?: string;
+	alt?: string;
 
 }
 
