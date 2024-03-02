2fbe668c48cd6946bcb412b770bc2a6e9a111b9c
PauloZupper <paulo.fernandes@zup.com.br>
2024-03-01 23:22:27 -0300

    feat/changing-to-taillwind: change project to taillwind

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,45 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
-import styledComponentsTransformer from 'typescript-plugin-styled-components'
+import tailwind from 'rollup-plugin-tailwindcss'
 
 const packageJson = require('./package.json')
 
-export default[
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file:packageJson.main,
-                format: 'cjs',
-                sourcemap: true
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({
-                tsconfig: './tsconfig.json',
-                transformers: [
-                    ()=>({before: [styledComponentsTransformer]})
-                ]
-            }),
-            postcss()
-        ],        
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{file: 'dist/index.d.ts', format: 'esm'}],
-        plugins: [dts()],
-        external: [/\.css$/]
-    }
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        transformers: []
+      }),
+      tailwind({
+        input: './src/input.css', // required
+        // Tailor the emitted stylesheet to the bundle by removing any unused CSS
+        // (highly recommended when packaging for distribution).
+        purge: false,
+      }),
+      postcss(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
 ]
